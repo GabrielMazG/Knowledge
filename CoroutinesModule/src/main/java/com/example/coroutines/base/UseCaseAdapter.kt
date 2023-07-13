219a61e1300d8b31a10ecff579c8feb9098691b2
Gabriel Alejandro Maz Bas <g.maz@globant.com>
2023-07-13 14:18:51 -0300

    Fix coroutines button text

--- CoroutinesModule/src/main/java/com/example/coroutines/base/UseCaseAdapter.kt
+++ CoroutinesModule/src/main/java/com/example/coroutines/base/UseCaseAdapter.kt
@@ -2,7 +2,7 @@ package com.example.coroutines.base
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coroutines.R
 
@@ -11,21 +11,19 @@ class UseCaseAdapter(
     private val onUseCaseClick: (UseCase) -> Unit
 ) : RecyclerView.Adapter<UseCaseAdapter.ViewHolder>() {
 
-    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class ViewHolder(val button: Button) : RecyclerView.ViewHolder(button)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val textView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.recyclerview_item, parent, false) as TextView
-        return ViewHolder(
-            textView
-        )
+        val button = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recyclerview_item, parent, false) as Button
+        return ViewHolder(button)
     }
 
     override fun getItemCount() = useCaseCategory.useCases.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = useCaseCategory.useCases[position].description
-        holder.textView.setOnClickListener {
+        holder.button.text = useCaseCategory.useCases[position].description
+        holder.button.setOnClickListener {
             onUseCaseClick(useCaseCategory.useCases[position])
         }
     }
