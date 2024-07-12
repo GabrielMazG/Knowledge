adb43afba6e4fc8669061b8848219b8920cdfa40
Gabriel Maz <g.maz@globant.com>
2024-07-12 09:42:31 +0200

    JetTrivia: Setup repository and api calls

---
+++ ComposeModule/src/main/java/com/example/composemodule/jettriviaapp/di/AppModule.kt
@@ -0,0 +1,31 @@
+package com.example.composemodule.jettriviaapp.di
+
+import com.example.composemodule.jettriviaapp.network.QuestionApi
+import com.example.composemodule.jettriviaapp.repository.QuestionRepository
+import com.example.composemodule.jettriviaapp.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideQuestionRepository(api: QuestionApi) = QuestionRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideQuestionApi(): QuestionApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(QuestionApi::class.java)
+    }
+}
